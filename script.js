8859cd7d2009753b63a18299d4a13f4db1266855
Isabela Biramontes <ibiramontes.student@hsfi.us>
2018-05-17 22:15:58 +0000

    navbar and api

--- script.js
+++ script.js
@@ -1,24 +1,10 @@
 /* global $*/
 $(document).ready(function(){
-    $.ajax({
-        url: "https://swapi.co/api/people/1",
-        method: "GET",
-        success: function(response) {
-            //Log the whole response to the console
-            console.log(response);
-            // Below write the code to print the name of the person 
-            console.log(response.name);
-            // Below write the code to print the mass of the person
-            console.log(response.mass);
-            // Below write the code to print the species of the person
-            console.log(response.species);
-            // Below write the code to print the starships of the person
-	        console.log(response.starships);
-    
-  },
+    $.get("div.html", function(data){
+            $("#OurNav").html(data);
+    });
+    $("#cssButton").click(function() {
+        alert( "Handler for .click() called." );
+    });
 }); 
     // Using code from playlist find some way to change the results of each card
-    
-    
-  
-});
