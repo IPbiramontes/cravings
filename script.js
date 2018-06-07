c1dd97e37ff8b46a796d51e235fb0894180cdd03
Isabela Biramontes <ibiramontes.student@hsfi.us>
2018-06-07 20:37:04 +0000

    finished?

--- script.js
+++ script.js
@@ -5,10 +5,21 @@ $(document).ready(function(){
     });
     $("#cssButton").click(function() {
         var userSearch = $("input").val();
-        userSearch === jb
+        var location =""
+        userSearch === location
      
-        
     });
+    $("#cssLoginButton").click(function() {
+        var username = $("#USER").val();
+        var password = $("#PASS").val();
+    if(username === "Isabela" && password === "Leslie" ){
+      alert("Congrats, you got in! but this feature don't even fully work yet");  
+    }
+    else{
+        alert("Try again");
+    }
+    });
+    
     console.log(review = "this \u2605 symbol");
     
 });
